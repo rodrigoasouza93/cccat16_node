69cd0433e8de2e8192fa7749e93b614633bfc9b8
Rodrigo Souza <rodrigo.augusto93@gmail.com>
2024-04-09 09:56:22 -0300

    refactor: validate cpf

---
+++ test/validateCpf.test.ts
@@ -0,0 +1,19 @@
+import { validate } from "../src/validateCpf";
+
+test.each([
+  "97456321558",
+  "71428793860",
+  "87748248800"
+])("should test a valid cpf: %s", function (cpf: any) {
+  expect(validate(cpf)).toBe(true);
+});
+
+test.each([
+  undefined,
+  null,
+  "11111111111",
+  "123",
+  "1234567891234567"
+])("should test a invalid cpf: %s", function (cpf: any) {
+  expect(validate(cpf)).toBe(false);
+});
